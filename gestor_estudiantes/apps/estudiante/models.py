f287b62fe5ad3d45bd924fca60e6f540983631be
E.M.Cirione.L <erikcirione@gmail.com>
2025-10-13 12:45:44 -0300

    tp terminado

--- gestor_estudiantes/apps/estudiante/models.py
+++ gestor_estudiantes/apps/estudiante/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 
-class Estudiante (models.Model):
-    nombreEstudiante= models.TextField (max_length=250, unique= True)
-    apellidoEstudiante= models.TextField (max_length=250, unique= True)
-    edadEstudiante= models.IntegerField (unique= True)
-    notaCursoEstudiante= models.IntegerField (blank=True)
-    cursoEstudiante= models.TextField (max_length=250, blank= True)
-
-    def _str_(self):
-        return f'nombre:{self.nombreEstudiante}, apellido:{self.apellidoEstudiante}, edad: {self.edadEstudiante}, nota: {self.notaCursoEstudiante}, curso: {self.cursoEstudiante}';
-
-
 
 class Curso (models.Model):
     nombreCurso= models.TextField (max_length=250, unique= True)
     cantidadHorasCurso= models.IntegerField ( unique= True)
 
-    def _str_(self):
+    def __str__(self):
         return f'nombre:{self.nombreCurso}, cantidad de horas:{self.cantidadHorasCurso}';
+
+
+class Estudiante (models.Model):
+    nombreEstudiante= models.TextField (max_length=250, unique= True)
+    apellidoEstudiante= models.TextField (max_length=250, unique= True)
+    edadEstudiante= models.IntegerField (unique= True)
+    notaCursoEstudiante= models.IntegerField (null=True )
+    cursoEstudiante= models.TextField (max_length=250, blank= True)
+    cursos= models.ManyToManyField(Curso, related_name='estudiantes')
+
+    def __str__(self):
+        return f'nombre:{self.nombreEstudiante}, apellido:{self.apellidoEstudiante}, edad: {self.edadEstudiante}, nota: {self.notaCursoEstudiante}, curso: {self.cursoEstudiante}';
+
+
 
 
 
